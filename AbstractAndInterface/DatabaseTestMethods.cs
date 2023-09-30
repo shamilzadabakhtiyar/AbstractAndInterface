498c14007585c906347ff123dd5052416838f805
Admin <Admin@DESKTOP-34CAHMM>
2023-10-01 01:06:56 +0400

    Add project files.

---
+++ AbstractAndInterface/DatabaseTestMethods.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractAndInterface
+{
+    public abstract class BaseDb
+    {
+        public BaseDb()
+        {
+            
+        }
+
+        public abstract string GetDbVersion();
+        public DataTable ExecuteSql(string sql)
+        {
+            return new DataTable();
+        }
+    }
+
+    public class MySqlDb : BaseDb
+    {
+        public override string GetDbVersion() => "MySql Database";
+
+        public string GenerateSql(int id)
+        {
+            return $"SLECET * FROM USERS WHERE ID = {id}";
+        }
+    }
+
+    public class SqlServerDb: BaseDb
+    {
+        public override string GetDbVersion() => "SqlServer 2019";
+
+        public string GenerateSql(int id)
+        {
+            return $"SLECET * FROM USERS WHERE ID = {id}";
+        }
+    }
+
+    public class OracleDb: BaseDb
+    {
+        public override string GetDbVersion() => "Oracle 18c";
+
+        public string GenerateSql(int id)
+        {
+            return $"SLECET * FROM USERS WHERE USER_ID = {id}";
+        }
+    }
+}
---
+++ AbstractAndInterface/VehicleTestMethods.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractAndInterface
+{
+    public interface IVehicle
+    {
+        void Go();
+        void Stop();
+    }
+
+    public interface IReadable
+    {
+        void Ride();
+    }
+
+    public interface IFlyable 
+    { 
+        void Soar();
+    }
+
+    public abstract class BaseVehicle: IVehicle
+    {
+        public void Go()
+        {
+            Console.WriteLine("Vehicle is going..");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("Vehicle is stopped..");
+        }
+    }
+
+    public class Car : BaseVehicle
+    {
+       
+    }
+
+    public class Bike : BaseVehicle, IReadable
+    {
+        public void Ride()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Plane : BaseVehicle, IFlyable
+    {
+        public void Soar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
